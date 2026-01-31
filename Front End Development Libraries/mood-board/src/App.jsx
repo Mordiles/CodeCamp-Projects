7d74304c6918d188df70b89a4d134b7ae6653c4c
Mordiles <rafifraditya21@gmail.com>
2026-01-31 15:34:40 +0700

    refactor App component to MoodBoard with MoodBoardItem integration

--- Front End Development Libraries/mood-board/src/App.jsx
+++ Front End Development Libraries/mood-board/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { MoodBoardItem } from "./components/MoodBoardItem";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function MoodBoard() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1 className="mood-board-heading">Destination Mood Board</h1>
+      <div className="mood-board">
+        <MoodBoardItem
+          color="#0000ff"
+          image="https://cdn.freecodecamp.org/curriculum/labs/pathway.jpg"
+          description="Beautiful Mountain with clear water"
+        />
+        <MoodBoardItem
+          color="#e3e3e3"
+          image="https://cdn.freecodecamp.org/curriculum/labs/shore.jpg"
+          description="Beautiful shore with clear water"
+        />
+        <MoodBoardItem
+          color="#ff44aa"
+          image="https://cdn.freecodecamp.org/curriculum/labs/grass.jpg"
+          description="Lush green grass in a peaceful meadow."
+        />
+        <MoodBoardItem
+          color="#23ff11"
+          image="https://cdn.freecodecamp.org/curriculum/labs/pigeon.jpg"
+          description="A cute pigeon on a bridge."
+        />
+        <MoodBoardItem
+          color="purple"
+          image="https://cdn.freecodecamp.org/curriculum/labs/ship.jpg"
+          description="A cool ship on a calm sea."
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default MoodBoard;
