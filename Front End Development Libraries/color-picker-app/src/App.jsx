8ae9db6376aebaf4fd43ba21e2bcef5c63a14930
Mordiles <rafifraditya21@gmail.com>
2026-02-02 12:25:52 +0700

    refactor: implement ColorPicker component with color input functionality

--- Front End Development Libraries/color-picker-app/src/App.jsx
+++ Front End Development Libraries/color-picker-app/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function ColorPicker() {
+  const [color, setColor] = useState("#ffffff");
+
+  const handleColorChange = (e) => {
+    setColor(e.target.value);
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <div id="color-picker-container" style={{ backgroundColor: color }} className="space-y-5">
+      <p>Choose a color using the input below:</p>
+      <input id="color-input" className="bg-white" type="color" value={color} onChange={handleColorChange} />
+    </div>
+  );
 }
 
-export default App
+export default ColorPicker;
